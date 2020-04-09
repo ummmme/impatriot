83127d953c5a7647eaaae998fd56d080f1296f12
Kenneh <Kenneh.fang@wetax.com.cn>
2020-04-09 20:44:03 +0800

    优化tls1.3版本并作为默认安装方式

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ V2Ray一键安装脚本
 
 ## Quick Start
 ```bash
-bash <(curl -f -L -sS https://raw.githubusercontent.com/abcfyk/impatriot/master/v2ray/ws_nginx_tls_1.3.sh)
+bash <(curl -f -L -sS https://raw.githubusercontent.com/abcfyk/impatriot/master/v2ray/ws_nginx_tls.sh)
 ```
 
 ## Note
 - 切勿在安装了$$的服务器上使用此脚本，切记切记
-- 推荐使用tls1.3脚本进行安装，安全性更好。但某些手机客户端的v2ray内核如果低于v4.18.1 可能不支持，请自行选择。
+- 现已默认使用tls1.3。某些手机客户端的v2ray内核如果低于v4.18.1 可能不支持，请自行选择。
 
 ## License
 MIT License
--- v2ray/ws_nginx_tls.sh
+++ v2ray/ws_nginx_tls.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
-# VPS一键安装V2ray脚本(websocket + nginx + tls)(Ubuntu18.04)
+# VPS一键安装V2ray脚本(使用TLS1.3，优化TLS1.2上的安全性问题)
 #0. 前言：必须先在dns服务商将二级域名指向新开的服务器，再在服务器上执行本脚本
-#1. 更新系统
-#2. 安装Nginx
+#1. 更新系统(Ubuntu18.04，19.04, Debian10 测试通过)
+#2. 编译安装Nginx + openssl
 #3. 申请证书：acme.sh
-#4. 安装V2ray, 配置生成：https://www.veekxt.com/utils/v2ray_gen
+#4. 安装V2ray
 #5. 安装完成后，将服务器上的/etc/v2ray/config.json.client 文件复制到本地的/etc/v2ray 文件夹下，并重命名为config.json后，重启本地v2ray即可
-#6. PS：实测使用websocket+nginx+tls模拟 正常访问网站已经足够绕过GFW识别，没有持续大流量访问的情况下，没有必要用Cloudfare的CDN
-#Date: 2019-06-16
-#2019-06-20: 增加一个404页面作为二级域名的主页
-#2019-06-27: 随机生成websocket的链接，随机生成字符串作为v2ray的链接，不再固定为ws
+#2020-03-19 修复使用Quick Start一键安装命令时，伪装首页无效的问题
+#2020-04-09 更新openssl版本为1.1.1f，优化兼容GCP等云服务器  
 
+
+NGINX_VERSION="1.17.9"
+OPENSSL_VERSION="1.1.1f"
 
 #说明
 showUsage() {
 cat 1>&2 <<EOF
 *-----------------------------------------------------------------------
-one key to install v2ray, nginx and apply tls cert script. usage:
-available args:
-[-d|--domain]: your vps domain name, pointing to current vps
-*-----------------------------------------------------------------------
-v2ray 一键安装脚本，自动安装v2ray, nginx, 自动申请证书，自动更新证书，自动生成websocket+nginx+tls模式的服务端和客户端配置
-使用方式：分别执行以下两行命令
-[1] cd /usr/local && git clone https://github.com/abcfyk/impatriot.git && cd impatriot/v2ray
-[2] bash ws_nginx_tls.sh -d 你的域名
+v2ray 一键安装脚本，自动安装v2ray, nginx, 自动申请证书，自动更新证书，自动生成websocket+nginx+tls1.3模式的服务端和客户端配置
 注意： 使用本脚本前必须先将域名指向这台服务器
 *-----------------------------------------------------------------------
 EOF
@@ -34,7 +28,7 @@ printr() {
 }
 
 # 生成随机数字
-function rand(){
+rand(){
     min=$1
     max=$(($2-$min+1))
     num=$(cat /proc/sys/kernel/random/uuid | cksum | awk -F ' ' '{print $1}')
@@ -42,45 +36,52 @@ function rand(){
 }
 
 #生成随机长度的字符串,默认为5到8位
-function randStr() {
+randStr() {
     len=`rand 5 8`;
     echo $(date +%s%N | md5sum | head -c ${len});
 }
 
+#等待输入域名，开始安装
+clear
+showUsage;
+read -p "$(echo "请输入您的域名，确保已经指向当前服务器：")" PROXY_DOMAIN;
 
-#获取参数
-PROXY_DOMAIN="";
-GET_ARGS=`getopt -o d: -al domain: -- "$@"`
-eval set -- "$GET_ARGS"
-
-#开始处理
-while [ -n "$1" ]
-do
-    case "$1" in
-        -d|--domain) PROXY_DOMAIN=$2; shift 2;;
-        --) break ;;
-        *) showUsage; break ;;
-    esac
-done
-
-#判断参数有效性:
-if [[ -z "${PROXY_DOMAIN}" ]]; then
-    showUsage;
-    exit 1;
-fi
-
+#判断域名有效性(兼容GCP等使用弹性IP的云服务器，只需要获取公网出口的IP地址即可，忽略代理层)
+PUBLIC_IP=$(curl ifconfig.me);
 CURRENT_IP=$(ifconfig -a | grep inet | grep -v "127.0.0.1\|inet6\|0.0.0.0" | awk '{print $2}' | tr -d "addr:");
 DOMAIN_IP=$(nslookup ${PROXY_DOMAIN} 8.8.8.8 | grep -v "8.8.8.8\|127.0.0.53" | grep "Address:"  | awk '{print $2}');
 
 if [[ "${CURRENT_IP}" != "${DOMAIN_IP}" ]]; then
-    printr "[ERROR]:  the domain: ${PROXY_DOMAIN} didn't point to current server.";
-    exit 1;
+    if [[ "${PUBLIC_IP}" != "${DOMAIN_IP}" ]]; then
+        printr "[ERROR]:  域名:${PROXY_DOMAIN} 没有指向当前服务器，请检查后重试";
+        exit 1;
+    fi
 fi
 
-#请勿修改以下配置-------------------------------------------
-#配置二级域名来转发v2ray流量，不要用一级域名
-PROXY_DOMAIN_CERT_FILE="/etc/nginx/ssl/${PROXY_DOMAIN}.fullchain.cer"
-PROXY_DOMAIN_KEY_FILE="/etc/nginx/ssl/${PROXY_DOMAIN}.key"
+#准备Nginx与Openssl的安装文件(暂不做签名比对)
+wget -c https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz -O /usr/local/nginx-${NGINX_VERSION}.tar.gz
+if [ ! -f "/usr/local/nginx-${NGINX_VERSION}.tar.gz" ]; then
+    printr "下载nginx失败，请重试"
+fi
+
+wget -c https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz  -O /usr/local/openssl-${OPENSSL_VERSION}.tar.gz
+if [ ! -f "/usr/local/openssl-${OPENSSL_VERSION}.tar.gz" ]; then
+    printr "下载openssl失败，请重试"
+fi
+
+#-----------------------------------------------------------------------------------------------------------------------
+# 请勿修改以下配置
+#-----------------------------------------------------------------------------------------------------------------------
+SHELL_DIR=`dirname $0`
+PROJECT_HOME=`cd ${SHELL_DIR}/..;pwd`
+
+#安装必要的组件
+sudo apt-get update
+sudo apt-get install -y build-essential libpcre3 libpcre3-dev zlib1g-dev unzip git dnsutils curl
+
+#配置三级域名来转发v2ray流量，不要用二级域名
+PROXY_DOMAIN_CERT_FILE="/usr/local/nginx/ssl/${PROXY_DOMAIN}.fullchain.cer"
+PROXY_DOMAIN_KEY_FILE="/usr/local/nginx/ssl/${PROXY_DOMAIN}.key"
 
 #UUID
 UUID=`cat /proc/sys/kernel/random/uuid`;
@@ -90,14 +91,10 @@ V2RAY_PATH=`randStr`;
 
 #0. 验证：
 #0.1 系统
-if [[ 'ubuntu' != "$(cat /etc/os-release | grep -w ID | awk -F '=' '{print $2}')" ]]; then
-    echo "System Not UBuntu, exit";
-    exit 1;
-fi
-
-#1. 基础配置 ：
-apt update
-apt -y upgrade
+#if [[ 'ubuntu' != "$(cat /etc/os-release | grep -w ID | awk -F '=' '{print $2}')" ]]; then
+#    echo "System Not UBuntu, exit";
+#    exit 1;
+#fi
 
 #1.0  机器名，时区
 #echo ${HOST_NAME} > /etc/hostname && hostname ${HOST_NAME}
@@ -120,7 +117,7 @@ source ~/.bashrc
 cp /usr/share/vim/vim80/defaults.vim /usr/share/vim/vim80/defaults.vim.bak
 #1.2.1 取消可视化模式(删除带有set mouse=a)
 lines=`grep -n 'set mouse=a' /usr/share/vim/vim80/defaults.vim | tail -1 | awk -F ":" '{print $1}'`;
-if [ ${lines} -gt 0 ]; then
+if [[ ${lines} -gt 0 ]]; then
     startM=`expr ${lines} - 1`;
     endN=`expr ${lines} + 1`;
     sed -i ${startM},${endN}d /usr/share/vim/vim80/defaults.vim
@@ -132,25 +129,79 @@ echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 sysctl net.ipv4.tcp_available_congestion_control
 
-#4. 安装Nginx
-#4.1 安装
-apt install -y nginx
+#4. 编译安装Nginx，开启tls1.3支持
+cd /usr/local
+
+#4.1.1 安装依赖
+groupadd www # 添加组
+useradd -s /sbin/nologin -g www www #添加用户
+
+#4.1.2 安装openssl
+tar zxvf openssl-${OPENSSL_VERSION}.tar.gz && rm openssl-${OPENSSL_VERSION}.tar.gz
+
+#4.1.3 下载nginx
+tar zxvf nginx-${NGINX_VERSION}.tar.gz && rm nginx-${NGINX_VERSION}.tar.gz
+cd nginx-${NGINX_VERSION}
+
+#编译
+./configure --user=www \
+--group=www \
+--prefix=/usr/local/nginx \
+--pid-path=/var/run/nginx.pid \
+--with-openssl=/usr/local/openssl-${OPENSSL_VERSION} \
+--with-openssl-opt='enable-tls1_3' \
+--with-http_v2_module \
+--with-http_ssl_module \
+--with-http_gzip_static_module \
+--with-http_stub_status_module \
+--with-http_sub_module \
+--with-stream \
+--with-stream_ssl_module
+
+#安装
+make && make install
+
+#快捷方式
+ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
+
+#Nginx注册服务(注意：Ubuntu的systemd路径与centos不一致)
+cat > /etc/systemd/system/nginx.service << EOF
+[Unit]
+Description=Nginx - high performance web server
+Documentation=http://nginx.org/en/docs/
+After=network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/local/nginx/sbin/nginx -t -c /usr/local/nginx/conf/nginx.conf
+ExecStart=/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+ExecReload=/bin/kill -s HUP `cat /var/run/nginx.pid`
+ExecStop=/bin/kill -s QUIT `cat /var/run/nginx.pid`
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# 启动nginx
+systemctl start nginx
 
 #4.2 配置nginx.conf, 默认主页为404页面
 mkdir -p /export/www/${PROXY_DOMAIN}
-if [ ! -f "../404/404.html" ]; then
-echo "hello" > /export/www/${PROXY_DOMAIN}/index.html
+if [[ ! -f "${PROJECT_HOME}/404/404.html" ]]; then
+    curl -f -L -sS https://raw.githubusercontent.com/abcfyk/impatriot/master/404/404.html > /export/www/${PROXY_DOMAIN}/index.html
 else
-cp ../404/404.html /export/www/${PROXY_DOMAIN}/index.html
+    cp ${PROJECT_HOME}/404/404.html /export/www/${PROXY_DOMAIN}/index.html
 fi
-mkdir -p /var/log/nginx
 
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-cat >  /etc/nginx/nginx.conf << EOF
-user  www-data;
+mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak
+cat >  /usr/local/nginx/conf/nginx.conf << EOF
+user  www;
 worker_processes  auto;
 
-error_log  /var/log/nginx/error.log warn;
+error_log  /usr/local/nginx/logs/error.log warn;
 pid        /var/run/nginx.pid;
 
 worker_rlimit_nofile 65535;
@@ -161,38 +212,20 @@ events {
 }
 
 http {
-    include       /etc/nginx/mime.types;
+    include       mime.types;
     default_type  application/octet-stream;
 
+    access_log  off;
     charset utf-8;
     client_header_buffer_size 32k; #上传文件大小限制
     large_client_header_buffers 4 64k; #设定请求缓
     client_max_body_size 8m; #设定请求缓存大小
-
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-
-    access_log  /var/log/nginx/access.log  main;
 
     sendfile        on;
     tcp_nopush      on;
     tcp_nodelay     on;
 
     keepalive_timeout  60;
-
-    #gzip模块设置
-    gzip               on;
-    gzip_vary          on;
-    gzip_comp_level    6;
-    gzip_buffers       16 8k;
-    gzip_min_length    1000;
-    gzip_proxied       any;
-    gzip_disable       "msie6";
-    gzip_http_version  1.0;
-    gzip_types         text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript;
-
-    include /etc/nginx/conf.d/*.conf;
 
     #站点配置
     server {
@@ -205,14 +238,14 @@ http {
 EOF
 
 #5. 重启
-service nginx restart
+systemctl restart nginx
 
 #6. 安装acme.sh 自动更新tls证书
 curl  https://get.acme.sh | sh
 source ~/.bashrc
 
 #6.1 创建证书存放文件夹
-mkdir -p /etc/nginx/ssl
+mkdir -p /usr/local/nginx/ssl
 
 #6.2 申请证书
 /root/.acme.sh/acme.sh  --issue -d ${PROXY_DOMAIN} --webroot /export/www/${PROXY_DOMAIN}
@@ -221,7 +254,7 @@ mkdir -p /etc/nginx/ssl
 /root/.acme.sh/acme.sh --installcert -d ${PROXY_DOMAIN} \
 --key-file ${PROXY_DOMAIN_KEY_FILE} \
 --fullchain-file ${PROXY_DOMAIN_CERT_FILE} \
---reloadcmd "service nginx force-reload"
+--reloadcmd "systemctl restart nginx"
 
 #6.4 自动更新证书
 /root/.acme.sh/acme.sh  --upgrade  --auto-upgrade
@@ -292,12 +325,12 @@ cat > /etc/v2ray/config.json << EOF
 EOF
 
 
-#6.2 更新Nginx的tls配置
-cat >  /etc/nginx/nginx.conf << EOF
-user  www-data;
+#6.2 更新Nginx的websocket + tls1.3 配置
+cat >  /usr/local/nginx/conf/nginx.conf << EOF
+user  www;
 worker_processes  auto;
 
-error_log  /var/log/nginx/error.log warn;
+error_log  /usr/local/nginx/logs/error.log warn;
 pid        /var/run/nginx.pid;
 
 worker_rlimit_nofile 65535;
@@ -309,7 +342,7 @@ events {
 }
 
 http {
-    include       /etc/nginx/mime.types;
+    include       mime.types;
     default_type  application/octet-stream;
 
     charset utf-8;
@@ -321,40 +354,27 @@ http {
 
     keepalive_timeout  60;
 
-    #gzip模块设置
-    gzip               on;
-    gzip_vary          on;
-    gzip_comp_level    6;
-    gzip_buffers       16 8k;
-    gzip_min_length    1000;
-    gzip_proxied       any;
-    gzip_disable       "msie6";
-    gzip_http_version  1.0;
-    gzip_types         text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript;
-
-    include /etc/nginx/conf.d/*.conf;
+    server {
+        listen 80;
+        server_name ${PROXY_DOMAIN};
+        rewrite ^(.*)$ https://\${server_name}\$1 permanent;
+    }
 
     #站点配置
     server {
-        listen  80;
-        server_name ${PROXY_DOMAIN};
-        rewrite ^(.*)$  https://\$host\$1 permanent;
-    }
-    server {
-        listen 443 ssl;
+        listen 443 ssl default_server;
 
-        ssl on;
         ssl_certificate       ${PROXY_DOMAIN_CERT_FILE};
         ssl_certificate_key   ${PROXY_DOMAIN_KEY_FILE};
-        ssl_protocols         TLSv1.1 TLSv1.2;
-        ssl_ciphers           EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;
+        ssl_protocols         TLSv1.3;
+        ssl_ciphers 'CHACHA20:EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH:ECDHE-RSA-AES128-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA128:DHE-RSA-AES128-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA128:ECDHE-RSA-AES128-SHA384:ECDHE-RSA-AES128-SHA128:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA128:DHE-RSA-AES128-SHA128:DHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA384:AES128-GCM-SHA128:AES128-SHA128:AES128-SHA128:AES128-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4;';
         ssl_prefer_server_ciphers on; #优化SSL加密
         ssl_session_cache shared:SSL:10m;
         ssl_session_timeout 60m;
 
         root /export/www/${PROXY_DOMAIN};
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
+        index index.html;
+        server_name ${PROXY_DOMAIN};
         location / {
             try_files \$uri \$uri/ =404;
         }
@@ -456,13 +476,13 @@ cat > /etc/v2ray/config.json.client << EOF
 EOF
 
 #6.4 重启nginx and v2ray
-service nginx restart
+systemctl restart nginx
 systemctl restart v2ray
 
 #7. 优化
 cat > /etc/sysctl.d/default.conf << EOF
 # 最大打开文件
-fs.file-max = 51200
+fs.file-max = 65535
 # 最大读取缓冲区
 net.core.rmem_max = 67108864
 # 最大写入缓冲区
@@ -518,6 +538,6 @@ sysctl --system
 
 #7.2 增加文件描述符限制, <所有用户> <软限制和硬限制> <文件描述符> <整型数值>
 mkdir -p /etc/security/limits.d
-echo "* - nofile 51200" > /etc/security/limits.d/default.conf;
+echo "* - nofile 65535" > /etc/security/limits.d/default.conf;
 
-echo "conguatulations. install finished, please copy the config file to your local machine.";
+echo "Conguatulations. install finished, please copy the file '/etc/v2ray/config.json.client' to your local machine.";
