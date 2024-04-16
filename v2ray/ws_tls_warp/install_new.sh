f156fa23728ca3411952b4c6056aa85435806f42
Kwan <8966854+ummmme@users.noreply.github.com>
2024-04-16 15:02:52 +0800

    Update install_new.sh for debian 12

--- v2ray/ws_tls_warp/install_new.sh
+++ v2ray/ws_tls_warp/install_new.sh
@@ -98,13 +98,13 @@ fi
 
 #1. 更新系统
 printr "1. UPDATING SYSTEM"
-curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-sudo apt update -qq && sudo apt upgrade -yqq
+curl https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+apt update -qq && apt upgrade -yqq
 
 #2. 安装必要的组件
 printr "2. INSTALLING REQUIREMENTS"
-sudo apt install -yqq build-essential libpcre3 libpcre3-dev zlib1g-dev unzip git dnsutils vim net-tools tcl tk expect
+apt install -yqq build-essential libpcre3 libpcre3-dev zlib1g-dev unzip git dnsutils vim net-tools tcl tk expect 
 
 #配置三级域名来转发v2ray流量，不要用二级域名
 PROXY_DOMAIN_CERT_FILE="/usr/local/nginx/ssl/${PROXY_DOMAIN}.fullchain.cer"
